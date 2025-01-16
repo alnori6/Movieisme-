e013b987c9d265c37b785defffa725fcd22a4c3a
noora amer <aoora6@gmail.com>
2025-01-16 14:29:35 +0300

    strating

---
+++ Movieisme/Movieisme/MovieismeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieismeApp.swift
+//  Movieisme
+//
+//  Created by Noori on 16/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct MovieismeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Movieisme/MovieismeTests/MovieismeTests.swift
@@ -0,0 +1,16 @@
+//
+//  MovieismeTests.swift
+//  MovieismeTests
+//
+//  Created by Noori on 16/01/2025.
+//
+
+import Testing
+
+struct MovieismeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
